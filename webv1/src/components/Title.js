3ca881588e3cd32e87b2519b4c72e59ddd65ce45
Diego García <71889821+diegogar99@users.noreply.github.com>
2021-04-12 12:40:55 +0200

    Delete webv1 directory

--- webv1/src/components/Title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-const Title = ({children}) => <h1 className="title">{children}</h1>
-
-Title.propTypes = {
-    children: PropTypes.string.isRequired
-}
-export default Title
